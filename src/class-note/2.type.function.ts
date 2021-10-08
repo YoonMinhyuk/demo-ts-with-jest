3e5a42d60e6ff56825919f34127092f0809dd602
minhyuk yoon <ymh92730@gmail.com>
2021-10-09 03:57:18 +0900

    [ Summary ] 1. "2.type.function.ts" 추가 - 함수 타입 정리
    [ Add ]
    1. "2.type.function.ts"

---
+++ src/class-note/2.type.function.ts
@@ -0,0 +1,16 @@
+// 함수의 파라미터에 타입을 정의하는 방식
+function sum(a: number, b: number) {
+    return a + b
+}
+
+const number1 = sum(1, 2);
+
+// 함수의 반환 값에 타입을 정의하는 방식
+function getNumber(): number {
+    return 10
+}
+
+// 위 두개의 결과로 도출되는 기본적인 함수의 최종 형태
+function add(a: number, b: number): number {
+    return a + b
+}
