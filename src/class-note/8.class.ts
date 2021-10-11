8b1588d576010d07ddd23107ce0c73a76d4df844
minhyuk yoon <ymh92730@gmail.com>
2021-10-11 20:25:25 +0900

    [ Summary ] 1. tsconfig 설정 추가 2. 8.class.ts compile error 수정
    [ Change ]
    1. 8.class.ts
    2. tsconfig.json

--- src/class-note/8.class.ts
+++ src/class-note/8.class.ts
@@ -1,14 +1,21 @@
 namespace TsClass {
     class Person {
+        private readonly name: string;
         public age: number;
         protected role: string
-        private readonly name: string;
         readonly readOnlyField: string
 
 
-        constructor(name: string, age: number) {
-            this.name = name;
-            this.age = age;
+        constructor(
+            name: string,
+            age: number,
+            role: string,
+            readOnlyField: string
+        ) {
+            this.name = name
+            this.age = age
+            this.role = role
+            this.readOnlyField = readOnlyField
         }
 
         public getRole(): string {
